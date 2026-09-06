41ce4be94ec8e9a118e926a5437c04f53ffe6397
Isha Dogra <isha15102004@gmail.com>
2026-09-06 22:24:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (50.95%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,int>set;
-       for(int  i = 0 ; i < t.length() ; i++){
-          set[t[i]]++;
-       }
+    //     unordered_map<char,int>set;
+    //    for(int  i = 0 ; i < t.length() ; i++){
+    //       set[t[i]]++;
+    //    }
       
-        for(int i = 0 ; i < s.length(); i++){
+    //     for(int i = 0 ; i < s.length(); i++){
            
-                 set[s[i]]--;
-        }
-        for(auto i : set){
-            if(i.second > 0){
-                return i.first;
-            }
-        }
-       return '\0';
+    //              set[s[i]]--;
+    //     }
+    //     for(auto i : set){
+    //         if(i.second > 0){
+    //             return i.first;
+    //         }
+    //     }
+    //    return '\0';
+
+    char ans = 0;
+    for(char ch : s){
+        ans^=ch;
+    }
+    for(char ch :t){
+        ans ^=ch;
+    }
+    return ans;
     }
 };
