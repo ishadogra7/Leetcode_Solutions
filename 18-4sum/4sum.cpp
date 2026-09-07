226d0fc8fed5e6e28ff2b8c47fe45cc63b70311e
Isha Dogra <isha15102004@gmail.com>
2026-09-07 23:25:23 +0530

    Time: 16 ms (87.38%) | Memory: 17.6 MB (12.52%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -23,12 +23,10 @@ public:
                         left++;
                         right--;
 
-                        while (left < right &&
-                               nums[left] == nums[left - 1])
+                        while (left < right && nums[left] == nums[left - 1])
                             left++;
 
-                        while (left < right &&
-                               nums[right] == nums[right + 1])
+                        while (left < right && nums[right] == nums[right + 1])
                             right--;
                     }
                     else if (sum < target) {
