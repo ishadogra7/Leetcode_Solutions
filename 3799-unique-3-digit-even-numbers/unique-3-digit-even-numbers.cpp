ab26821bf255a05cfa98227d025819ea8f397e7b
Isha Dogra <isha15102004@gmail.com>
2026-09-11 13:41:28 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (100.00%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
+  int freq[10]={};
+  int count = 0;
+    void solve (int position){
+        if(position == 3){
+            count++;
+            return;
+        }
+        for(int digit = 0 ;digit<= 9;digit++){
+            if(freq[digit] == 0) continue;  // digit not available
+            if(position == 0 && digit == 0)continue; // for fist position no 0 digit
+            if(position == 2 && digit %2 != 0)continue;
+
+            freq[digit]--; //use digit
+            solve(position + 1); //recursively choose the next digit
+            freq[digit]++; //backtrack..put the digit back
+        }
+    }
     int totalNumbers(vector<int>& digits) {
-        int freq[10]={};
-        for(int x : digits){
-            freq[x]++;
+        
+        for(int digit : digits){
+            freq[digit]++;
         }
-        int count = 0;
-
-        for(int first = 1; first<= 9; first++){
-            if(freq[first] == 0) continue;
-
-            freq[first]--;
-
-            for(int second = 0 ; second <= 9 ; second++){
-                if(freq[second] == 0 ) continue;
-
-                freq[second]--;
-
-                for(int third = 0 ; third <=8 ; third +=2){
-                    if(freq[third] >0) count ++;
-                }
-               freq[second]++;
-            }
-            freq[first]++;
-        }
+        solve(0);
         return count;
     }
 };
