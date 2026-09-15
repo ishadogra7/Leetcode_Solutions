a68f7577e3271ab3c4748e9065bc448a8ff029f3
Isha Dogra <isha15102004@gmail.com>
2026-09-15 22:32:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (35.09%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,31 @@
+#include<algorithm>
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+      map<char , int> result;
+      for(int i = 0 ; i<s.size();i++){
+        result[s[i]] = i;
+      }
+
+      set<char> used;
+      string st;
+
+      for(int i = 0 ; i< s.size() ;i++){
+        char ch = s[i];
+
+        if(used.find(ch) != used.end()){
+            continue;
+        }
+
+        while(!st.empty()&&  st.back() >ch &&  result[st.back()] > i){
+            used.erase(st.back());
+            st.pop_back();      
+            
+              }
+
+            st.push_back(ch);
+            used.insert(ch);
+      }
+      return st;
+    }
+};
