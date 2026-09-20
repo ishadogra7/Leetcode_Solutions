e431cadea984e118bee96a74bb6e3985267e6077
Isha Dogra <isha15102004@gmail.com>
2026-09-20 21:21:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (95.77%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-       int pro = 1;
+     
        int add = 0;
        for(int i = 1 ; i <= s.length(); i++){
         
-           pro = (26- (s[i-1] - 'a') ) *  i ;  
+           int pro = (26- (s[i-1] - 'a') ) *  i ;  
            add += pro;
        } 
        return add;
